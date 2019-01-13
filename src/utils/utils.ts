9c9c088f8a252277644ef4231abce81463e349ab
Mircea MATEI <matei_tm@yahoo.com>
2019-01-14 01:42:19 +0200

    #7. Add utils class for entity name manipulation

---
+++ src/utils/utils.ts
@@ -0,0 +1,11 @@
+class Utils {
+    static getEntityNamePluralsInPascalCase(entityNameInPascalCase: string): any {
+      throw new Error("Method not implemented.");
+    }
+    static getEntityNameInCamelCase(entityNameInPascalCase: string): any {
+      throw new Error("Method not implemented.");
+    }
+    static getUnderscoreCase(entityNameInPascalCase: string): any {
+      throw new Error("Method not implemented.");
+    }
+  }
