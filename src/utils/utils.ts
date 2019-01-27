c04321f1829fe7f10b2b453141df04841a7cb683
Mircea MATEI <matei_tm@yahoo.com>
2019-01-28 01:53:32 +0200

    #9. Adding pluralize package. Light implementation for all utils methods

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,27 @@
+const pluralize = require("pluralize");
+
 export class Utils {
-  static getEntityNamePluralsInPascalCase(entityNameInPascalCase: string): any {
-    throw new Error("Method not implemented.");
+  static getEntityNamePluralsInPascalCase(entityNameInPascalCase: string): string {
+    var expandWithSpaces = entityNameInPascalCase.replace(/([a-z])([A-Z])/g, '$1 $2');
+    var wordsArray = expandWithSpaces.split(' ');
+
+    wordsArray[wordsArray.length - 1] = pluralize(wordsArray[wordsArray.length - 1]);
+
+    var entityNamePlural = wordsArray.join('');
+
+    return entityNamePlural;
   }
-  static getEntityNameInCamelCase(entityNameInPascalCase: string): any {
-    throw new Error("Method not implemented.");
+
+  static getEntityNameInCamelCase(entityNameInPascalCase: string): string {
+    return entityNameInPascalCase.substr(0, 1).toUpperCase() + entityNameInPascalCase.substr(1);
   }
-  static getUnderscoreCase(entityNameInPascalCase: string): any {
-    throw new Error("Method not implemented.");
+
+  static getUnderscoreCase(entityNameInPascalCase: string): string {
+    return entityNameInPascalCase.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase();;
+  }
+
+  static getUnderscoreCasePlural(entityNameInPascalCase: string): string {
+    var entityNamePluralsInPascalCase = Utils.getEntityNamePluralsInPascalCase(entityNameInPascalCase);
+    return entityNamePluralsInPascalCase.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase();
   }
 }
