30a2d85ad27fd893e46be50fb53c14a200a5caa7
Mircea MATEI <matei_tm@yahoo.com>
2019-02-26 21:37:36 +0200

    #9. Major refactoring moved some functional code to OOP

---
+++ src/parser/model_parser.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class ModelParser {
+    private modelsFolderPath: fs.PathLike;
+    private modelFileName: string;
+    private modelFullPath: fs.PathLike;
+
+    constructor(modelsFolderPath: fs.PathLike, modelFileName: string) {
+        this.modelsFolderPath = modelsFolderPath;
+        this.modelFileName = modelFileName;
+        this.modelFullPath = path.join(modelsFolderPath.toString(), modelFileName);
+    }
+
+    async initialize() {
+        console.log(`Start parsing existing model: ${this.modelsFolderPath}`);
+        await fs.readFile(this.modelFullPath, (err, file) => { file.forEach((line) => console.log(line)); });
+    }
+
+    async getModelName(): Promise<string> {
+        if (this.modelFileName === null) {
+           await this.initialize();
+        }
+
+        return this.modelFileName;
+    }
+}
