bb9f0cd45e89ec2dca1a5bb81bf3922f60da01a8
Mircea MATEI <matei_tm@yahoo.com>
2019-04-10 04:54:49 +0300

    #25. package.json review

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,7 @@ import { DatabaseType } from './helper/database_type';
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "flutter-sqlite-generator" is now active!');
+	console.log('Congratulations, your extension "flutter-orm-m8-generator" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
